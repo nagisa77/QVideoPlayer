82f8dc4fc48a1bbe95127777bd6cf23baf073e64
timjtchen <timjtchen@tencent.com>
2023-12-13 09:41:07 +0800

    Initial Commit

---
+++ QVideoPlayer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  QVideoPlayer
+//
+//  Created by jt on 2023/12/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
