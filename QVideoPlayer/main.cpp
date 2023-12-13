95080ea0cb3714ede214e7edf42f2a440edca9a4
timjtchen <timjtchen@tencent.com>
2023-12-13 10:13:32 +0800

    feat: init

--- QVideoPlayer/main.cpp
+++ QVideoPlayer/main.cpp
@@ -6,9 +6,12 @@
 //
 
 #include <iostream>
+#include <libavcodec/avcodec.h>
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
-  return 0;
+  //这里简单的输出一个版本号
+  std::cout << "Hello FFmpeg!" << std::endl;
+  unsigned version = avcodec_version();
+  std::cout << "version is:" << version;
+  return 0;  return 0;
 }
