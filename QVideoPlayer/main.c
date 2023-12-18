9edc19448cf7e01019731bc22b5f7cbc9939e5fe
timjtchen <timjtchen@tencent.com>
2023-12-18 18:45:24 +0800

    feat: 解码帧，并保存

--- QVideoPlayer/main.c
+++ QVideoPlayer/main.c
@@ -10,8 +10,9 @@
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
 #include <png.h>
+#include <sys/time.h>
 
-int save_frame_to_png(const char *filename, AVFrame *frame) {
+int save_frame_to_png_origin(const char *filename, AVFrame *frame) {
     // 打开PNG文件
     FILE *fp = fopen(filename, "wb");
     if (!fp) {
@@ -47,7 +48,14 @@ int save_frame_to_png(const char *filename, AVFrame *frame) {
 
     // 写PNG文件头信息
     png_init_io(png_ptr, fp);
-    png_set_rows(png_ptr, info_ptr, frame->data);
+//    png_set_rows(png_ptr, info_ptr, frame->data);
+    png_bytep *row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * frame->height);
+    for (int y = 0; y < frame->height; y++) {
+        row_pointers[y] = frame->data[0] + y * frame->linesize[0];
+    }
+    free(row_pointers);
+
+    png_set_rows(png_ptr, info_ptr, row_pointers);
     png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
 
     // 清理PNG结构体
@@ -55,6 +63,36 @@ int save_frame_to_png(const char *filename, AVFrame *frame) {
 
     // 关闭文件
     fclose(fp);
+
+    return 0;
+}
+
+int save_frame_to_png(const char *filename, AVFrame *frame) {
+    // Convert the frame to RGB format
+    struct SwsContext *sws_ctx = sws_getContext(frame->width, frame->height, frame->format,
+                                                 frame->width, frame->height, AV_PIX_FMT_RGB24,
+                                                 SWS_BILINEAR, NULL, NULL, NULL);
+    if (!sws_ctx) {
+        fprintf(stderr, "Could not initialize the conversion context\n");
+        return -1;
+    }
+
+    AVFrame *rgb_frame = av_frame_alloc();
+    rgb_frame->format = AV_PIX_FMT_RGB24;
+    rgb_frame->width = frame->width;
+    rgb_frame->height = frame->height;
+    av_frame_get_buffer(rgb_frame, 0);
+
+    sws_scale(sws_ctx, (const uint8_t *const *)frame->data, frame->linesize, 0,
+              frame->height, rgb_frame->data, rgb_frame->linesize);
+
+    // Save the RGB frame as a PNG image
+    // ... (use the same code as in your original save_frame_to_png function)
+    save_frame_to_png_origin(filename, rgb_frame);
+
+    // Free resources
+    av_frame_free(&rgb_frame);
+    sws_freeContext(sws_ctx);
 
     return 0;
 }
@@ -94,36 +132,50 @@ int main(int argc, const char * argv[]) {
   AVCodecContext* pCodecCtx = avcodec_alloc_context3(codec);
   avcodec_parameters_to_context(pCodecCtx, pFormatCtx->streams[video_stream_index]->codecpar);
   
+  if (avcodec_open2(pCodecCtx, codec, NULL) < 0) {
+      fprintf(stderr, "Could not open codec\n");
+      return -1;
+  }
+  
   AVPacket pkt;
   AVFrame* frame = av_frame_alloc();
   uint64_t idx = 0;
+  
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+  uint64_t frameCount = 0;
+  
   while (av_read_frame(pFormatCtx, &pkt) >= 0) {
     if (pkt.stream_index == video_stream_index) {
       if (avcodec_send_packet(pCodecCtx, &pkt)) {
-        while (avcodec_receive_frame(pCodecCtx, frame)) {
-          printf("width=%d, height=%d, format=%d\n", frame->width, frame->height, frame->format);
+        int ret = avcodec_receive_frame(pCodecCtx, frame);
+        if (ret == 0) {
 
-          int width = frame->width;
-          int height = frame->height;
-          if (width <= 0 || height <= 0) {
-            // printf("无效的帧大小：width=%d, height=%d\n", width, height);
-            continue;
-          }
-          const char* save_dir = "/Users/chenjiating/Downloads/QVideoPlayer/frame";
-          char filename[256];
-          memset(filename, 0, sizeof(filename));
-          sprintf(filename, "%s/frame%d.png", save_dir, idx++);
-          save_frame_to_png(filename, frame);
-          break;
+            int width = frame->width;
+            int height = frame->height;
+            if (width <= 0 || height <= 0) {
+                // printf("无效的帧大小：width=%d, height=%d\n", width, height);
+                continue;
+            }
+            const char* save_dir = "/Users/jt/Downloads/frames";
+            char filename[256];
+            memset(filename, 0, sizeof(filename));
+            sprintf(filename, "%s/frame%d.png", save_dir, idx++);
+          
+            printf("width=%d, height=%d, format=%d\n", frame->width, frame->height, frame->format);
+            save_frame_to_png(filename, frame);
+          
+          frameCount++;
         }
       }
     }
-//    if (pkt.stream_index == audio_stream_index) {
-//      //
-//    }
     av_packet_unref(&pkt);
   }
   
+  gettimeofday(&end, NULL);
+  double elapsedTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+  printf("Decoded frames: %llu\n", frameCount);
+  printf("Elapsed time: %.2f seconds\n", elapsedTime);
 
   // 释放AVFormatContext
   avformat_close_input(&pFormatCtx);
